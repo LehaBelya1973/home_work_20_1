fbd3661df268e9a52696c8f2f083d963937c3a30
Aleksey Belyaev <lesha.belyaev.73@mail.ru>
2024-01-16 16:28:21 +0300

    Для каждой модели описал свои поля и установил библиотеку Pillow

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,24 +1,36 @@
 from django.db import models
-from django.utils import timezone
 
 
 NULLABLE = {'blank': True, 'null': True}
 
 
 class Category:
-    name_category =
-    description_category =
-    pass
-    pass
+    name_category = models.CharField(max_length=100, verbose_name='Категория')
+    description_category = models.TextField(verbose_name='Описание категории', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name_category}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('name_category',)
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='название')
-    description = models.TextField(verbose_name='Изображение', **NULLABLE)
-    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
     product_category = models.ForeignKey(Category, verbose_name='Категория')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за единицу')
     date_of_production = models.DateField(auto_now_add=True, verbose_name='Дата внесения товара', **NULLABLE)
     date_last_changes = models.DateField(auto_now=True, verbose_name='Дата последнего изменения', **NULLABLE)
 
+    def __str__(self):
+        return f'{self.name} {self.price} {self.product_category}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ('name', 'price')
 
