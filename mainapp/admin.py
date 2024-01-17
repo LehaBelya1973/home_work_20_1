3a996bcf36a0cf036eec967665787325e221572f
Aleksey Belyaev <lesha.belyaev.73@mail.ru>
2024-01-17 13:53:19 +0300

    Переписал код в admin.py по заданию

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -5,10 +5,12 @@ from mainapp.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description',
-                    'product_category', 'price', 'date_of_production')
+    list_display = ('pk', 'name',
+                    'price', 'product_category')
+    list_filter = ('product_category',)
+    search_fields = ('name', 'description')
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name_category', 'description_category')
+    list_display = ('pk', 'name_category')
