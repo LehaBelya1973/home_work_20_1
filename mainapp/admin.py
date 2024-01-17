aa4d707ce567c4ff7e71bc525f776d0783d40e59
Aleksey Belyaev <lesha.belyaev.73@mail.ru>
2024-01-17 10:50:38 +0300

    Создал и применил миграции для новых моделей

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from mainapp.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description',
+                    'product_category', 'price', 'date_of_production')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name_category', 'description_category')
--- mainapp/models.py
+++ mainapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
-class Category:
+class Category(models.Model):
     name_category = models.CharField(max_length=100, verbose_name='Категория')
     description_category = models.TextField(verbose_name='Описание категории', **NULLABLE)
 
@@ -21,7 +21,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
     description = models.TextField(verbose_name='Описание', **NULLABLE)
     image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
-    product_category = models.ForeignKey(Category, verbose_name='Категория')
+    product_category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за единицу')
     date_of_production = models.DateField(auto_now_add=True, verbose_name='Дата внесения товара', **NULLABLE)
     date_last_changes = models.DateField(auto_now=True, verbose_name='Дата последнего изменения', **NULLABLE)
