a6c5f9c23ae28df897b813a54b1ca4fcef35de9a
Aleksey Belyaev <lesha.belyaev.73@mail.ru>
2024-01-26 16:22:20 +0300

    Описание товаров сократил до 100 символов

--- mainapp/models.py
+++ mainapp/models.py
@@ -19,7 +19,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
-    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    description = models.TextField(max_length=100, verbose_name='Описание', **NULLABLE)
     image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за единицу')
