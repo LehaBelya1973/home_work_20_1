3500a7090e4ae223858e658e6a056f99b94607cd
Aleksey Belyaev <lesha.belyaev.73@mail.ru>
2024-01-26 20:22:12 +0300

    Создал шаблон products.html, куда будут помещены все тровары выбранной категории

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from mainapp.apps import MainappConfig
-from mainapp.views import index, categories
+from mainapp.views import index, categories, category_product
 
 app_name = MainappConfig.name
 
 urlpatterns = [
     path('', index, name='index'),
     path('categories/', categories, name='categories'),
+    path('<int:pk>/mainapp/', category_product, name='category_product'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -18,3 +18,11 @@ def categories(request):
     }
     return render(request, 'main/categories.html', context)
 
+
+def category_product(request, pk):
+    category_item = Category.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(category_id=pk),
+        'title': f'Товары из категории {category_item.name}',                             
+    }
+    return render(request, 'main/products.html', context)
